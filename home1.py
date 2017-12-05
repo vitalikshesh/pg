71a30479174e6bf3aeea61aaea8b1bd1af270f2a
vitalikshesh <32903126+vitalikshesh@users.noreply.github.com>
2017-12-05 13:57:17 +0200

    home1.py

---
+++ home1.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+#! /usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+name = str(input('Введіть ваше ім’я: '))
+fath = str(input('Введіть ваше побатькові: '))
+print('Вітаю, ' + name + ' ' + fath + '!')
+# pg
